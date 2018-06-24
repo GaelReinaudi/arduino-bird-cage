dab8a3fc7355fa6742418e76f6af9887a7a9e46a
Gael Reinaudi <gael.reinaudi@gmail.com>
2018-06-24 12:54:23 -0400

    mods

--- clickDoors/clickDoors.ino
+++ clickDoors/clickDoors.ino
@@ -6,6 +6,7 @@ Servo servoFront;
 const int pinServoLeft = 2;
 const int pinServoFront = 1;
 const int buttonPin = 15;
+const int ledPin =  16;
 
 const int posLeftOpen = 195;
 const int posLeftClose = 10;
@@ -13,7 +14,7 @@ const int posFrontOpen = 10;
 const int posFrontClose = 195;
 
 const int posRefMax = 100;
-int pos = 0;
+int pos = posRefMax / 2;
 
 int posLeft(int posRef)
 {
@@ -23,17 +24,20 @@ int posFront(int posRef)
 {
   return posFrontOpen + (posRef * (posFrontClose - posFrontOpen)) / posRefMax;
 }
-void updateServo(int pos)
+void updateServo(int posRef)
 {
-    servoLeft.write(posLeft(pos));
-    servoFront.write(posFront(pos));
+    servoLeft.write(posLeft(posRef));
+    servoFront.write(posFront(posRef));
 }
 
 void setup() 
 { 
   servoLeft.attach(pinServoLeft);
   servoFront.attach(pinServoFront);
-  pinMode(buttonPin, INPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+  updateServo(pos);
+  digitalWrite(ledPin, LOW);
 }
 
  
@@ -41,23 +45,30 @@ void loop()
 { 
   int button = digitalRead(buttonPin);
   int incPos = 0;
-  if (!button)
-    return;
+//  if (!button == LOW)
+//    return;
+  if (button == HIGH) {
+    digitalWrite(ledPin, LOW);
+  } else {
+    digitalWrite(ledPin, HIGH);
+  }
     
-  if (pos <= posRefMax / 20)
+  if (pos <= posRefMax / 2 && button == HIGH)
     incPos = 1;
-  if (posRefMax - pos <= posRefMax / 20)
+  if (posRefMax - pos <= posRefMax / 2 && button == HIGH)
     incPos = -1;
 
-  servoLeft.attach(pinServoLeft);
-  servoFront.attach(pinServoFront);
+//  servoLeft.attach(pinServoLeft);
+//  servoFront.attach(pinServoFront);
 
-  while(pos > 0 and pos < posRefMax) {
+  while(pos > 0 and pos < posRefMax)
+  {
     pos += incPos;
-    updateServo(pos);
+    servoLeft.write(pos);
     delay(15);
   }
-  servoLeft.detach();
-  servoFront.detach();
+  incPos = 0;
+//  servoLeft.detach();
+//  servoFront.detach();
 } 
 
